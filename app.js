4a2619f98b7ee3f30be3467bd085ad73e7243e15
Sam <re711kimo@gmail.com>
2020-05-13 23:19:40 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose') // 載入 mongoose
+const exphbs = require('express-handlebars')
+
 const app = express()
 const port = 3000
 
-const exphbs = require('express-handlebars')
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+const db = mongoose.connection // 取得資料庫連線狀態
+
+db.on('error', () => {
+  console.log('mongodb error!') // 連線異常
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!') // 連線成功
+})
+
 const restaurantList = require('./restaurant.json')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -11,23 +25,23 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.render('index', { restaurant: restaurantList.results})
+  res.render('index', { restaurant: restaurantList.results })
 })
 
 app.get('/restaurant/:restaurant_id', (req, res) => {
-    const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-    res.render('show', { restaurant: restaurant})
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
 })
 
 app.get('/search', (req, res) => {
-    const keyword = req.query.keyword
-    const restaurant = restaurantList.results.filter(restaurant => {
-        return restaurant.name.toLocaleLowerCase().includes(keyword.toLocaleLowerCase()) ||
-            restaurant.category.toLocaleLowerCase().includes(keyword.toLocaleLowerCase())
-    })
-    res.render('index', { restaurant: restaurant, keyword: keyword})
+  const keyword = req.query.keyword
+  const restaurant = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLocaleLowerCase().includes(keyword.toLocaleLowerCase()) ||
+    restaurant.category.toLocaleLowerCase().includes(keyword.toLocaleLowerCase())
+  })
+  res.render('index', { restaurant: restaurant, keyword: keyword })
 })
 
 app.listen(port, () => {
-    console.log(`Express is listening on localhost:${port}`)
+  console.log(`Express is listening on http://localhost:${port}`)
 })
