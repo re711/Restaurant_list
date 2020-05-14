ab6ebe24a033b61a662b9e8e91515f09418565af
Sam <re711kimo@gmail.com>
2020-05-14 22:43:05 +0800

    feat: add restaurant seeder

--- models/restaurant.js
+++ models/restaurant.js
@@ -5,13 +5,37 @@ const restaurantSchema = new Schema({
     type: String, // 資料型別是字串
     required: true // 這是個必填欄位
   },
-  name_en: String,
-  category: String,
-  image: String,
-  location: String,
-  phone: String,
-  google_map: String,
-  rating: String,
-  description: String
+  name_en: {
+    type: String,
+    require: true
+  },
+  category: {
+    type: String,
+    require: true
+  },
+  image: {
+    type: String,
+    require: true
+  },
+  location: {
+    type: String,
+    require: true
+  },
+  phone: {
+    type: String,
+    require: true
+  },
+  google_map: {
+    type: String,
+    require: true
+  },
+  rating: {
+    type: String,
+    require: true
+  },
+  description: {
+    type: String,
+    require: true
+  }
 })
 module.exports = mongoose.model('Restaurant', restaurantSchema)
