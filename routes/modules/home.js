875e624e1c13aef8a3488858d9dcc8a8e394383e
Sam <re711kimo@gmail.com>
2020-05-20 13:55:19 +0800

    feat: add home sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -15,8 +15,18 @@ router.get('/search', (req, res) => {
   const keyword = req.query.keyword
   Restaurant.find({ $or: [{ name: new RegExp(keyword, 'i') }, { category: new RegExp(keyword, 'i') }] })
     .lean()
-    .sort()
     .then(restaurants => res.render('index', { restaurants, keyword }))
+    .catch(error => console.log(error))
+})
+
+// 排序功能
+router.get('/:sorting/:order', (req, res) => {
+  const sorting = req.params.sorting
+  const order = req.params.order
+  Restaurant.find()
+    .lean()
+    .sort({ [sorting]: [order] })
+    .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.log(error))
 })
 module.exports = router
